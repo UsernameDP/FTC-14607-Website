ac8abf923570dcc3efa01d865a0601e61c24152d
Devin Park <devinpark345@gmail.com>
2023-11-12 12:06:13 -0500

    TODO : Add google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,28 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route
-        path="/*"
-        element={<App />}
-      />
-    </Routes>
-  </BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//Initialize GA4
+ReactGA.initialize("G-6QYHQ8S1HD");
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/*"
+          element={<App />}
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+const SendAnalytics = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname
+  });
+};
